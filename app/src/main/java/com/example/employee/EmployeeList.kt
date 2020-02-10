34fd37d7013844b285cc7c4c6c08db27481d98d0
Chancie Johnson <chancie.johnson@enhanceit.us>
2020-02-10 02:38:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/employee/EmployeeList.kt
@@ -0,0 +1,12 @@
+package com.example.employee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmployeeList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_employee_list)
+    }
+}
---
+++ app/src/main/java/com/example/employee/Employees.kt
@@ -0,0 +1,13 @@
+package com.example.employee
+
+data class Employees(
+    var firstName : String,
+    var lastName : String,
+    var streetAddress : String,
+    var city : String,
+    var state : String,
+    var zip: String,
+    var taxId : String,
+    var position : String,
+    var department : String
+)
